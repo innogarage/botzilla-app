e59affc7f6f03c332a024e4422e875123294a3ca
andreibotezatu <andrei.botezatu@innogarage.com>
2015-03-03 18:11:37 +0200

    added .gitignore

---
+++ js/app.js
@@ -0,0 +1,20 @@
+(function() {
+
+
+    var avatar, token, toolbar_title;
+
+
+
+    toolbar_title = "Welcome in the Botzilla world";
+    avatar = "images/abyssinian.jpg";
+
+    token = "385778e421960d0028c68d8cd4fdd5a22f185a6e";
+
+    var template = document.querySelector('template[is=auto-binding]');
+
+    template.token = token;
+    template.toolbar_title = toolbar_title;
+    template.avatar = avatar;
+
+
+})();
